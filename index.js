f63063888e779c745cf3038ae6ac14cc17f51e3d
Simcha Schaum <simcha.schaum@gmail.com>
2021-11-09 16:08:28 +0200

    added footer, added transitions to header

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const navbar = document.querySelector('.navbar');
 const brand = document.querySelector(".navbar-brand");
+const navLink = document.querySelectorAll(".nav-link");
 
 window.onscroll = function(){
     scrollFunction()
 }
 
 function scrollFunction(){
-    if(document.body.scrollTop > 75 || document.documentElement.scrollTop > 75){
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
         navbar.style.fontSize = "1.25em";
         navbar.style.height = "50px";
-        brand.style.width = "3em";
-
+        brand.style.width = "2em";
+        navLink.forEach(item => {
+            item.style.height = "30px"
+        })
     } else {
         navbar.style.fontSize = "1.50em";
         navbar.style.height = "125px";
-        brand.style.width = "5em"
+        brand.style.width = "5em";
+        navLink.forEach(item => {
+            item.style.height = "52px"
+        })
     }
 }
