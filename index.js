0e8b2c781dba4caf906ed58bd52b6fc1510fe491
Simcha Schaum <simcha.schaum@gmail.com>
2021-11-11 12:57:10 +0200

    fixed hiding navbar in mobile and navbar formatting issues

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const navbar = document.querySelector('.navbar');
 const brand = document.querySelector(".navbar-brand");
 const navLink = document.querySelectorAll(".nav-link");
 
+let width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
 window.onscroll = function(){
-    scrollFunction()
+        scrollFunction()
 }
 
 function scrollFunction(){
     if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
         navbar.style.fontSize = "1.25em";
-        navbar.style.height = "50px";
-        brand.style.width = "2em";
-        navLink.forEach(item => {
-            item.style.height = "30px"
-        })
+        navbar.style.height = "60px";
+        if(width>975){
+            navLink.forEach(item => {
+                item.style.height = "30px"
+            });
+            brand.style.width = "2em";
+        } else {
+            brand.style.width = "1.5em";
+        }
+
     } else {
         navbar.style.fontSize = "1.50em";
         navbar.style.height = "125px";
